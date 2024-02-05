9b2fc0cf0186693ccb870c1b3f36c5499d9e8c45
Paralaxe <andre.bagatelli@paralaxe.com.br>
2024-02-05 16:59:27 -0300

    init front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConnectionApiService } from './connection-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+
+  constructor(private connectionService: ConnectionApiService){}
+
+  public mensagemDoBackend: string = '';
+
+  objetoParaEnviar = {
+    chave1: 'valor-incorreto',
+    chave2: 'valor2'
+  };
+
+  ngOnInit(): void {
+    this.sendData()
+    this.getData()
+  }
+
+  getData() {
+    this.connectionService.buscarDados().subscribe(
+      (data) => {
+        this.mensagemDoBackend = data.mensagem;
+        console.log("mensagem:", this.mensagemDoBackend)
+      },
+      (error) => {
+        console.error('Erro ao chamar a API:', error);
+      }
+    )
+  }
+
+  sendData() {
+    this.connectionService.enviarObjeto(this.objetoParaEnviar).subscribe(
+      (resposta) => {
+        console.log('Resposta do backend:', resposta);
+      },
+      (erro) => {
+        console.error('Erro ao enviar objeto para o backend:', erro);
+      }
+    );
+  }
 }
